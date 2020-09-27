bfc74c2454b31d122a7f3634b9470171946d8328
Dipto <diptobiswas1998@gmail.com>
2020-09-28 00:13:41 +0530

    [Dipto] refactor: Adding Conditions

--- WageProblem/Program.cs
+++ WageProblem/Program.cs
@@ -18,23 +18,46 @@ namespace Welcome
             empWage = empHr * EmpRatePerHour;
             int empFTWageMonth = 0;
             int empPTWageMonth = 0;
+            int totalPTTime = 0;
+            int totalFTTime = 0;
+            int i = 1;
 
-            for (int i = 1; i <= 20; i++)
+            for (i = 1; i <= 20; i++)
             {
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
 
                 if (empCheck == IsPartTime)
                 {
+                    totalPTTime = totalPTTime + empHr;
+                    if (totalPTTime > 100)
+                    {
+                        totalPTTime = 100;
+                        empPTWageMonth = 2000;
+                    }
                     empPTWageMonth = empPTWageMonth + empWage;
                 }
                 else if (empCheck == IsFullTime)
                 {
+                    totalFTTime = totalFTTime + empHr;
+                    if (totalFTTime > 100)
+                    {
+                        totalFTTime = 100;
+                        empFTWageMonth = 2000;
+                    }
                     empFTWageMonth = empFTWageMonth + empWage;
                 }
             }
-            Console.WriteLine("Part time Employee Monthly Wage = " + empPTWageMonth);
-            Console.WriteLine("Full time Employee Monthly Wage = " + empFTWageMonth);
+            Console.WriteLine("Part time Employee Monthly Wage = " + empPTWageMonth + ", Hours worked = " + totalPTTime);
+            if (totalPTTime < 100)
+            {
+                Console.WriteLine("Days worked = " + (i - 1));
+            }
+            Console.WriteLine("Full time Employee Monthly Wage = " + empFTWageMonth + ", Hours worked = " + totalFTTime);
+            if (totalFTTime < 100)
+            {
+                Console.WriteLine("Days worked = " + (i - 1));
+            }
         }
     }
 }
