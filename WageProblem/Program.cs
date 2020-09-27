b650161caf9c35f03d7b750db236f32f1b325b0f
Dipto <diptobiswas1998@gmail.com>
2020-09-27 23:14:49 +0530

    [Dipto] refactor: Checking if Employee is present or absent

--- WageProblem/Program.cs
+++ WageProblem/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace WageProblem
+namespace Welcome
 {
     class Program
     {
@@ -8,6 +8,18 @@ namespace WageProblem
         {
             //Welcome msg
             Console.WriteLine("Welcome to the Employee Wage Problem!");
+            //Check
+            int IsFullTime = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IsFullTime)
+            {
+                Console.WriteLine("Employee is Present.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent!");
+            }
         }
     }
 }
