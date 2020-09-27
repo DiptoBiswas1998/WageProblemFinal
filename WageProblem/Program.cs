d4ff86cdcc2d16f3790902b3ded8238a25c40f5e
Dipto <diptobiswas1998@gmail.com>
2020-09-27 23:28:47 +0530

    [Dipto] refactor: Calculating Daily Wage

--- WageProblem/Program.cs
+++ WageProblem/Program.cs
@@ -8,18 +8,27 @@ namespace Welcome
         {
             //Welcome msg
             Console.WriteLine("Welcome to the Employee Wage Problem!");
-            //Check
+            //Solution
             int IsFullTime = 1;
+            int EmpRatePerHour = 20;
+
+            int empWage = 0;
+            int empHr = 0;
+
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == IsFullTime)
             {
                 Console.WriteLine("Employee is Present.");
+                empHr = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent!");
+                empHr = 0;
             }
+            empWage = empHr * EmpRatePerHour;
+            Console.WriteLine("Employee Wage = " + empWage);
         }
     }
 }
